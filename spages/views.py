b6d09332d96e308fe7f3915f94cd86569f5ba93b
harshalbabu <harshalbabu45@gmail.com>
2022-01-27 18:58:48 +0530

    start of app

--- spages/views.py
+++ spages/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_page(request):
-    return HttpResponse("helloworld")
+    # return HttpResponse("helloworld")
+    return render(request, "spages/home_page.html")
