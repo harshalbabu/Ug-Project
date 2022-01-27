6c6c9bcff7b30f75715fd367e9b4c0ef9a03675c
harshalbabu <harshalbabu45@gmail.com>
2022-01-27 19:15:56 +0530

    second website

--- airport_website/urls.py
+++ airport_website/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from spages.views import home_page
+from booking.views import booking_details
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page)
+    path('', home_page),
+    path('booking/', booking_details),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def booking_details(request):
+    return render(request, "flight_details.html")
